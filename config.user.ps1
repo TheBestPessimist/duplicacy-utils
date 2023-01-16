17575f33f7de1f86269d17f75593aa3b5a26526c
stanly bunker <stanly61315@gmail.com>
2023-01-16 16:09:29 +0800

    support multiple scheduled task using systemd

--- config.user.ps1
+++ config.user.ps1
@@ -18,7 +18,7 @@
 # $repositoryFolder = "C:/duplicacy repositories/my downloads/"
 
 # Full path to Duplicacy.exe
-# $duplicacyExePath = "C:\Duplicacy\.duplicacy\duplicacy_win_x64_2.1.1.exe"
+$duplicacyExePath = "/home/stanly/.local/bin/duplicacy"
 
 # Should the "-d" flag (debuging) be used? (hint: it generally shouldn't)
 # $duplicacyDebug = $true
